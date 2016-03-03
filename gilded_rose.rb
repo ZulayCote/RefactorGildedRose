a9e3ba30c68d6bd0cb9baed50b4d82e69a4f4100
zulay <andreyttha@hotmail.com>
2016-03-03 18:50:10 -0500

    Clase AgedBrie

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -60,6 +60,24 @@ end
 class SulfurasHandler < BaseItemHandler
 end
 
+class AgedBrieHandler < BaseItemHandler
+  private
+
+  def update_quality
+    if item.sell_in > 0
+      quality = item.quality + 1
+    else
+      quality = item.quality + 2
+    end
+
+    item.quality = quality if quality <= 50
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+end
+
 
 class GildedRose
 
