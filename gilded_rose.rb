55c4d8610836621d17fbcedc0ac24b14ebefa6ec
zulay <andreyttha@hotmail.com>
2016-03-03 18:34:51 -0500

    Clase Base

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,5 +1,26 @@
 require './item.rb'
 
+class BaseItemHandler
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+    update_quality
+    update_sell_in
+  end
+
+  private
+
+  def update_quality
+  end
+
+  def update_sell_in
+  end
+end
+
 class GildedRose
 
   def update_quality(items)
