65523a644b01cc0a8292da8ca91d41241e8203f4
zulay <andreyttha@hotmail.com>
2016-03-03 18:41:11 -0500

    Clase Base

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -21,6 +21,26 @@ class BaseItemHandler
   end
 end
 
+class DefaultItemHandler < BaseItemHandler
+  private
+
+  def update_quality
+    if item.sell_in > 0
+      quality = item.quality - 1
+    else
+      quality = item.quality - 2
+    end
+
+    item.quality = quality if quality >= 0
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+end
+
+
+
 class GildedRose
 
   def update_quality(items)
