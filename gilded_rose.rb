0ea42d9cf9fa6c5de35951b9fbf5756f1f555e9f
zulay <andreyttha@hotmail.com>
2016-03-03 18:46:41 -0500

    Clase Backstage

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -38,7 +38,24 @@ class DefaultItemHandler < BaseItemHandler
   end
 end
 
+class BackstagePassesHandler < BaseItemHandler
+  private
 
+  def update_quality
+    quality = if item.sell_in > 10
+                item.quality + 1
+              elsif item.sell_in > 5
+                item.quality + 2
+              elsif item.sell_in > 0
+                item.quality + 3
+              else
+                0
+              end
+
+    item.quality = quality if quality <= 50
+    item.quality = 50 if quality > 50
+  end
+end
 
 class GildedRose
 
